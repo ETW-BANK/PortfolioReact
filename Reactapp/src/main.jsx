d7b5908815508d72549285c0e194ec94b939d69e
dinozoff1981 <tensaeg@yahoo.com>
2024-03-27 23:45:20 +0100

    pages

--- Reactapp/src/main.jsx
+++ Reactapp/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-   <BrowserRouter basename='/PortfolioReact'>
+   <BrowserRouter basename='/PortfolioReact-1/reactapp'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
